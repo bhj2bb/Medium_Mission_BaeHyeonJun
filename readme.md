de23d40f5d923b062729d7aa83b373be0ab89202
bhj2bb <cs2uee@gmail.com>
2023-12-12 14:34:27 +0900

    로그인, 로그아웃

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 - [x] GET /member/join : 가입 폼
 - [x] POST /member/join : 가입 폼 처리
 ### 로그인
-- [ ] GET /member/login : 로그인 폼
-- [ ] POST /member/login : 로그인 폼 처리
+- [x] GET /member/login : 로그인 폼
+- [x] POST /member/login : 로그인 폼 처리
 ### 로그아웃
-- [ ] POST /member/logout : 로그아웃
+- [x] POST /member/logout : 로그아웃
--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ public class MemberController {
     private final MemberService memberService;
     private final Rq rq;
 
+    @PreAuthorize("isAnonymous()")
     @GetMapping("/join")
     public String showJoin() {
         return "domain/member/member/join";
@@ -35,10 +37,16 @@ public class MemberController {
         private String password;
     }
 
+    @PreAuthorize("isAnonymous()")
     @PostMapping("/join")
     public String join(@Valid JoinForm joinForm) {
         RsData<Member> joinRs = memberService.join(joinForm.getUsername(), joinForm.getPassword());
 
         return rq.redirectOrBack(joinRs, "/member/login");
+    }
+
+    @GetMapping("/login")
+    public String showLogin() {
+        return "domain/member/member/login";
     }
 }
